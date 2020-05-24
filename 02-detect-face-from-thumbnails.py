f216f172933617a4c421965364ae65394e57c8dc
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-05-24 22:25:40 +0900

    Update the script to detect faces in thumbnails

--- 02-detect-face-from-thumbnails.py
+++ 02-detect-face-from-thumbnails.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import sys
 import csv
 import urllib
+import argparse
 from PIL import Image
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
@@ -16,26 +18,38 @@ def count_faces_of(url):
     return boxes.shape[0] if boxes is not None else 0
 
 def main():
-    import sys
-    in_file = 'all-videos-20200507.csv'
-    out_file = 'videos-face-20200507.csv'
-    with open(in_file, 'r') as ifp, open(out_file, 'w') as ofp:
-        csv_reader = csv.reader(ifp)
-        header_row = next(csv_reader)
-        ncol = len(header_row)
-        url_col = header_row.index('thumbnail-url')
-        vid_col = header_row.index('vid')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('in-file', type=argparse.FileType('r'),
+                        help='csv file has cols of "video-id,thumbnail-url"')
+    parser.add_argument('out-file', type=argparse.FileType('w'),
+                        help='csv file added column "n-faces"')
 
-        csv_writer = csv.writer(ofp)
-        csv_writer.writerow(header_row + ['n-faces'])
+    args = parser.parse_args()
+    ifp = args.__dict__['in-file']
+    ofp = args.__dict__['out-file']
+    verbose = args.verbose
 
-        n_rows = 0
-        for row in csv_reader:
-            vid, url = row[vid_col], row[url_col]
-            n_rows += 1
-            nfaces = count_faces_of(url)
-            csv_writer.writerow(row + [nfaces])
+    csv_reader = csv.reader(ifp)
+    header_row = next(csv_reader)
+    ncol = len(header_row)
+    url_col = header_row.index('thumbnail-url')
+    vid_col = header_row.index('vid')
+
+    csv_writer = csv.writer(ofp)
+    csv_writer.writerow(header_row + ['n-faces'])
+
+    n_rows = 0
+    for row in csv_reader:
+        vid, url = row[vid_col], row[url_col]
+        n_rows += 1
+        nfaces = count_faces_of(url)
+        csv_writer.writerow(row + [nfaces])
+        if verbose:
             sys.stdout.write(f'\r{n_rows}')
+
+    ifp.close()
+    ofp.close()
 
 if __name__ == '__main__':
     main()
