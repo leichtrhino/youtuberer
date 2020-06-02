6175c625af99175c1f031a32d3b9d1358fa86577
leichtrhino <yoshikimethod@hotmail.co.jp>
2020-06-02 21:59:29 +0900

    Add script to generate confidence score and face embedding for each video

---
+++ 05-postprocess.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import os
+import re
+import math
+import pickle
+import argparse
+
+import numpy as np
+import torch
+import torchvision.io
+
+from facenet_pytorch import InceptionResnetV1
+
+def calculate_confidence(activesd_path):
+    with open(activesd_path, 'rb') as fp:
+        dists = pickle.load(fp)
+    confidences = []
+    for dist in dists:
+        mean_dists = np.mean(dist, 0)
+        minval = np.min(mean_dists)
+        conf = np.median(mean_dists) - minval
+        confidences.append(conf)
+    return confidences
+
+def calculate_embedding(video_path, model, device):
+    vframes, _, _ = torchvision.io.read_video(video_path, 0., 1., 'sec')
+    vframes = vframes.to(device)
+    frames = vframes.permute((0, 3, 1, 2)).float().div(255)
+    frames = torch.nn.functional.interpolate(frames, (160, 160))
+    embedding_vectors = model(frames).detach()
+    embedding, _ = embedding_vectors.median(0)
+    return embedding.to('cpu').numpy()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', default='cpu')
+    parser.add_argument('--without-embedding', action='store_true')
+    parser.add_argument('--without-confidence', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('syncnet-output-dir')
+
+    args = parser.parse_args()
+    syncnet_output_dir = args.__dict__['syncnet-output-dir']
+    pywork_dir = os.path.join(syncnet_output_dir, 'pywork')
+    pycrop_dir = os.path.join(syncnet_output_dir, 'pycrop')
+    video_ids = list(filter(
+        lambda x: re.match(r'[\w\-]{8,12}', x),
+        set(os.listdir(pywork_dir)) & set(os.listdir(pycrop_dir))
+    ))
+
+    device = torch.device(args.device)
+    model = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+    for vid in video_ids:
+        if args.verbose:
+            print(f'processing {vid}')
+        activesd_path = os.path.join(pywork_dir, vid, 'activesd.pckl')
+        if not args.without_confidence:
+            confidence_file = os.path.join(pywork_dir, vid, 'confidence.pckl')
+            confidence = calculate_confidence(activesd_path)
+            with open(confidence_file, 'wb') as fp:
+                pickle.dump(confidence, fp)
+
+        if not args.without_embedding:
+            scene_names = sorted(os.listdir(os.path.join(pycrop_dir, vid)))
+            get_scene_file = lambda x: os.path.join(pycrop_dir, vid, x)
+            embedding_file = os.path.join(pywork_dir, vid, 'embeddings.pckl')
+            embeddings = [
+                calculate_embedding(get_scene_file(scene_name), model, device)
+                for scene_name in scene_names
+            ]
+            with open(embedding_file, 'wb') as fp:
+                pickle.dump(embeddings, fp)
+
+if __name__ == '__main__':
+    main()
